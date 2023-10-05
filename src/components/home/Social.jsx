8b6be1db8917ae88ff9838d3d2e855024b01077d
Sharaneshwar Bharat Punjal <punjalsharaneshwar@gmail.com>
2023-10-05 11:15:33 +0530

    fixed target section

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -5,19 +5,19 @@ import twitter from '../../assets/twitter.png'
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://github.com/Sharaneshwar" className="home__social-icon" target="_blank">
-                <i class="uil uil-github-alt"></i>
+            <a href="https://github.com/Sharaneshwar" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-github-alt"></i>
             </a>
 
-            <a href="https://www.linkedin.com/in/sharaneshwar-punjal/" className="home__social-icon" target="_blank">
-                <i class="uil uil-linkedin-alt"></i>
+            <a href="https://www.linkedin.com/in/sharaneshwar-punjal/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-linkedin-alt"></i>
             </a>
 
-            <a href="https://twitter.com/Sharan_2208" className="home__social-icon" target="_blank">
+            <a href="https://twitter.com/Sharan_2208" className="home__social-icon" target="_blank" rel="noreferrer">
                 <img src={twitter} alt='twitter' />
             </a>
 
-            <a href="https://www.hackerrank.com/sharaneshwar" className="home__social-icon" target="_blank">
+            <a href="https://www.hackerrank.com/sharaneshwar" className="home__social-icon" target="_blank" rel="noreferrer">
                 <img src={hackerrank} alt='hackerrank' />
             </a>
 
